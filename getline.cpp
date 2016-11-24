6d311b7f22ee60e11a2df28d69b3ff5b723874bd
Egria <ned1_32@sina.com>
2016-11-24 20:27:44 +0800

    Updated with Error

--- getline.cpp
+++ getline.cpp
@@ -4,6 +4,10 @@
 #include <fstream>
 using namespace std;
 int main(int argc,char *argv[]){
+    if (argc<2){
+                cout<<"Please run as: mips.exe filename.in (filename.out)"<<endl;
+                return 0;
+                }
     ifstream in;
     ofstream out;
     in.open(argv[1]);
@@ -11,14 +15,17 @@ int main(int argc,char *argv[]){
        out.open(argv[2]);}
     else{out.open("code.txt");}
     string s;
+    int line=0;
     while (getline(in,s)){
           char* p = (char*)s.c_str();
           unsigned short mycode=MyAsm(p);
+          if (mycode<0) {cout<<"Error: Wrong Instruction in line "<<line<<". Program halt."<<endl; return 0;}
           //sendOneWord(com,mycode);
           if (mycode<4096) out<<"0";
           if (mycode<256) out<<"0";
           if (mycode<16) out<<"0";
           out<<hex<<mycode;
+          line++;
          }
     in.close();
     out.close();
