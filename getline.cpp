9df62e554cf3deb3147b84dc8852260cbd595523
Egria <ned1_32@sina.com>
2016-11-24 20:13:22 +0800

    updated: delete /r/n, add argv

--- getline.cpp
+++ getline.cpp
@@ -3,20 +3,25 @@
 #include <string>
 #include <fstream>
 using namespace std;
-int main(){
+int main(int argc,char *argv[]){
     ifstream in;
-   // ofstream out;
-    in.open("test.txt");
-   // out.open("code.txt");
+    ofstream out;
+    in.open(argv[1]);
+    if (argc>2){
+       out.open(argv[2]);}
+    else{out.open("code.txt");}
     string s;
     while (getline(in,s)){
           char* p = (char*)s.c_str();
           unsigned short mycode=MyAsm(p);
-          sendOneWord(com,mycode);
-          //out<<hex<<mycode<<endl;
+          //sendOneWord(com,mycode);
+          if (mycode<4096) out<<"0";
+          if (mycode<256) out<<"0";
+          if (mycode<16) out<<"0";
+          out<<hex<<mycode;
          }
     in.close();
-    //out.close();
+    out.close();
    // system("pause");
     return 0;
 }
